4fb93b7221a2ae3ce1d6a6a968a8861c2b666384
Muneer <muneerskoppam1107@gmail.com>
2025-07-18 11:57:05 +0530

    vercel setting

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-// Routes 
+// routes 
 app.use('/api/user', userRouter);
 app.use('/api/questions', questionRoutes);
 app.use('/api/submit', submitRoutes);
